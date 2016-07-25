bd6e4f00cb54ee5fde6b96fc45dbee8351b4fb97
widzew <klop99@wp.pl>
2016-07-25 15:18:32 +0200

    version 0.7 - kalkulator

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -6,15 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int result = 2 + 2;
 
         System.out.println("Hello");
-        System.out.println("What`s your name?");
+        System.out.println("Adding or docking?");
 
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
+        input.nextInt();
 
-        System.out.println(name + " your result is :" + result);
+        int y = input.nextInt();
+        int x = input.nextInt();
+        int result = x + y;
+
+        int result1 = x - y;
+
+
+        if (name.equals("Adding")) {System.out.println("your result is:" + result);}
+        if (name.equals("docking")) {System.out.println("your result is:" + result1);}
     }
 }
